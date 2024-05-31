10fd515e9675bceb973fc83e4cb52a1dcdfcb61e
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-05-31 09:56:38 +0200

    milestone 1

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const eventsRouter = require("./routers/eventsRouter")
 
+app.use(express.json());
+
+app.use("/events", eventsRouter);
 
 app.listen(3000, () => {
     console.log('Server attivo sulla porta http://localhost:3000.');
